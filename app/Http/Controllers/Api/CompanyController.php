c24fef9ae2b2aca1207400101e82fa8ad0470d36
jefcoro02 <jefcoronel2@gmail.com>
2025-01-14 19:29:09 -0500

    Primera config

---
+++ app/Http/Controllers/Api/CompanyController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $companies = Company::where('user_id', auth()->id())->get();
+
+        return response()->json($companies, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'razon_social' => 'required|string||max:255',
+            'ruc' => 'required|string',
+            'direccion' => 'required|string|max:255',
+            'logo' => 'nullable|file|image',
+            'sol_user' => 'required|string|max:255',
+            'sol_pass' => 'required|string|max:255',
+            'cert' => 'required|file|mimes:pem,txt',
+            'client_id' => 'nullable|string|max:255',
+            'client_secret' => 'nullable|string|max:255',
+            'production' => 'nullable|boolean',
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $data['logo_path'] = $request->file('logo')->file('logos');
+        }
+
+        $data['cert_path'] = $request->file('cert')->file('certs');
+        $data['user_id'] = auth()->id();
+
+        $company = Company::create($data);
+
+        return response()->json([
+            'message' => 'Empresa creada con exito',
+            'company' => $company
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Company $company)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company)
+    {
+        //
+    }
+}
